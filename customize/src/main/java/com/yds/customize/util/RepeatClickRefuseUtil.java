519b730a1451666a5b5faf2f57947a15f83e87b6
simon <simon_19900501@163.com>
2021-02-02 14:28:15 +0800

    增加：清除缓存工具 修改：重复点击工具

--- customize/src/main/java/com/yds/customize/util/RepeatClickRefuseUtil.java
+++ customize/src/main/java/com/yds/customize/util/RepeatClickRefuseUtil.java
@@ -5,6 +5,7 @@ package com.yds.customize.util;
  * @date 2020/8/18
  * @discribe 防止 同一时间段内多次点击 工具
  */
+@SuppressWarnings("all")
 public class RepeatClickRefuseUtil {
     private static int DELAY_TIME = 2000;                 //点击间隔延迟2000毫秒
 
@@ -17,17 +18,38 @@ public class RepeatClickRefuseUtil {
      * @param listener
      */
     public static void clickButton(OnClickListener listener) {
+        calculateTime(DELAY_TIME, listener);
+    }
+
+    /**
+     * 点击按钮事件-自定义时间
+     *
+     * @param delayTime
+     * @param listener
+     */
+    public static void clickButton(int delayTime, OnClickListener listener) {
+        calculateTime(delayTime, listener);
+    }
+
+    /**
+     * 时间计算
+     *
+     * @param delayTime
+     * @param listener
+     */
+    private static void calculateTime(int delayTime, OnClickListener listener) {
         //获取两次点击事件得时间差
         long timeDifference = System.currentTimeMillis() - lastClickTime;
         //如果时间差大于我们设定的点击间隔时间，则允许用户点击
-        if (timeDifference > DELAY_TIME) {
+        if (timeDifference > delayTime) {
             isCanClick = true;
         } else {
             isCanClick = false;
+            listener.onClickRefuse();
         }
         if (isCanClick) {
             lastClickTime = System.currentTimeMillis();
-            listener.onClick();
+            listener.onClickEnabled();
         }
     }
 
@@ -35,6 +57,10 @@ public class RepeatClickRefuseUtil {
      * 允许点击事件回调
      */
     public interface OnClickListener {
-        void onClick();
+        void onClickEnabled();
+
+        default void onClickRefuse() {
+
+        }
     }
 }
